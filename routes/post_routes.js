65fe983b4bedc336bf6de898b5c226e75d74c712
Your Name <you@example.com>
2024-11-22 12:59:00 +0200

    adding get by post id

--- routes/post_routes.js
+++ routes/post_routes.js
@@ -3,8 +3,7 @@ const router  = express.Router()
 const post = require('../controllers/posts')
 
 router.get('/',post.getAllPosts)
-
+router.get('/:id',post.getPostById)
 router.post('/',post.createPost)
-
 
 module.exports = router;
